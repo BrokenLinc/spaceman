2a9932512290dfa583e8df632e564381d2fe8d09
Lincoln Anderson <landerson@threefivetwo.com>
2023-09-24 00:20:14 -0400

    update button ux

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -1,7 +1,10 @@
 extends CanvasLayer
 
+@onready var startButton = %StartButton;
+
 func _ready():
   process_mode = Node.PROCESS_MODE_ALWAYS
+  startButton.grab_focus()
 
 func _on_start_button_pressed():
   Globals.start_game()
--- scenes/PauseOverlay.gd
+++ scenes/PauseOverlay.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+@onready var mainMenuButton = %MainMenuButton;
+@onready var resumeButton = %ResumeButton;
+
 func _ready():
   process_mode = Node.PROCESS_MODE_ALWAYS
   %Root.hide()
@@ -10,6 +13,7 @@ func _ready():
 
 func _on_game_paused():
   %Root.show()
+  resumeButton.grab_focus()
 
 
 func _on_game_resumed():
@@ -18,3 +22,7 @@ func _on_game_resumed():
 
 func _on_main_menu_button_pressed():
   Globals.load_main_menu()
+
+
+func _on_resume_button_pressed():
+  Globals.set_paused(false)
